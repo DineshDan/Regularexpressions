52b5304dd23a105160aaa29a76ffadd9c66d411a
DineshDan <dineshdanchai341@gmail.com>
2020-11-26 16:08:37 +0530

    Added: Logic to check the name is valid or invalid

--- registration.sh
+++ registration.sh
@@ -1,1 +1,9 @@
 echo "Welcome to the user regitrstion program being solved in the master branch"
+read -p "enter your first name: " firstname
+firstnameRegex="^[A-Z]{1}[a-z]{2,}"
+if(($firstname =~ $firstnameRegex))
+then
+      echo "$firstname is a valid name"
+else
+     echo "invalid name"
+fi
