1e9ded807358e81608e373e281407abbfa6f7e4e
DineshDan <dineshdanchai341@gmail.com>
2020-11-26 16:59:00 +0530

    Added: Logic to check have atleat one uppercase in password

--- registration.sh
+++ registration.sh
@@ -1,9 +1,9 @@
 echo "Welcome to the user regitrstion program being solved in the master branch"
 read -p "enter atleat 8 characters password:" password
-passwordRegex="^[0-9a-zA-Z]{8,}$"
+passwordRegex="^[0-9a-zA-Z{1,}]{8,}$"
 if(($characters =~ $PasswordRegex))
 then
-      echo "$Password have atleast 8 characters"
+      echo "$Password have atleast 1 uppercase"
 else
      echo "invalid password"
 fi
