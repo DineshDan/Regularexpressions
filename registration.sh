015bef61d020a29d80a766d2e1ef454f1962f186
DineshDan <dineshdanchai341@gmail.com>
2020-11-26 16:20:10 +0530

    Added: Logic to check EmailId is valid or not

--- registration.sh
+++ registration.sh
@@ -1,9 +1,9 @@
 echo "Welcome to the user regitrstion program being solved in the master branch"
-read -p "enter your last name: " lastname
-lastnameRegex="[A-Z]{1}[a-z]{2,}$"
-if(($lastname =~ $lastnameRegex))
+read -p "enter a valid emailid:" emailId
+emailIdRegex="^[a-zA-Z0-9\-\+\.]*.([a-zA-Z0-9])?@([a-z0-9]*.[a-z]{2,4}.([a-z]{2,})?)$"
+if(($emailId =~ $emailIdRegex))
 then
-      echo "$lastname is a valid name"
+      echo "$emailId is a valid name"
 else
-     echo "invalid name"
+     echo "invalid emailId"
 fi
