9fd4054c1668d8f9576015efee9c5f0a2d322220
DineshDan <dineshdanchai341@gmail.com>
2020-11-26 17:37:52 +0530

    Added: Logic to check whether user entered password is valid or not

--- registration.sh
+++ registration.sh
@@ -1,9 +1,9 @@
 echo "Welcome to the user regitrstion program being solved in the master branch"
-read -p "enter atleat 8 characters password:" password
-passwordRegex="^[0-9a-zA-Z{1,}]{8,}$"
-if(($characters =~ $PasswordRegex))
+read -p "enter a valid password:" password
+passwordRegex="(?=.{8,}$)(?=.*\d)(?=.*[A-Z])[a-z0-9A-Z]*[\@\#\^]"
+if(($password =~ $passwordRegex))
 then
-      echo "$Password have atleast 1 uppercase"
+      echo "$Password is a valid password"
 else
      echo "invalid password"
 fi
